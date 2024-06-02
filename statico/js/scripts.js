0bf64a1f168aa7220daa8610d9ed364e0ddad641
gabrielabermejo <98917590+gabrielabermejo@users.noreply.github.com>
2024-06-02 03:28:11 -0300

    Update scripts.js

--- statico/js/scripts.js
+++ statico/js/scripts.js
@@ -1,1 +1,25 @@
+function convertCode() {
+    const inputCode = document.getElementById('input-code').value;
+    const direction = document.getElementById('conversion-direction').value;
 
+    fetch('/convert', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ code: inputCode, direction: direction })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.converted_code) {
+            document.getElementById('output-code').value = data.converted_code;
+        } else if (data.error) {
+            document.getElementById('output-code').value = 'Error: ' + data.error;
+        } else {
+            document.getElementById('output-code').value = 'Unexpected response';
+        }
+    })
+    .catch(error => {
+        document.getElementById('output-code').value = 'Request failed: ' + error;
+    });
+}
